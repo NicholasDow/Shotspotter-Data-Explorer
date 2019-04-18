165c2e11ccba7de0fa24b0b4b747e9ac531c889d
unknown <dow@college.harvard.edu>
2019-04-18 15:34:13 -0400

    was not practical to use palm beach coords were not easy to use, using goldsboro NC

--- script.R
+++ script.R
@@ -6,10 +6,21 @@ library(leaflet)
 library(tigris)
 api_key <- "ad6d908d59b8fd6b7cd711901f628f9060feb972"
 census_api_key(api_key)
-palm_beach <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/04/palmbeachcounty_fl.csv")
+Goldsboro <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/04/goldsboro_nc.csv")
 
-fl <- counties(state = "FL", class = "sf")
-fl <-  fl[fl$NAME == "Palm Beach",]
+raw_shapes <- urban_areas(class = "sf")
+
+shapes <- raw_shapes %>% 
+  filter(NAME10 == "Goldsboro, NC")
+
+# do this manipulation in the app
+Goldsboro_locations <- st_as_sf(Goldsboro, 
+                                 coords = c("longitude", "latitude"), 
+                                 crs = 4326)
+
+write_rds(Goldsboro, "Goldsboro.rds")
+write_rds(shapes, "shapes.rds") 
 
 
-fl %>% ggplot() + geom_sf()
+
+shapes %>% ggplot() + geom_sf() + geom_sf(data = Goldsboro_locations)
