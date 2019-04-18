ce812873d69e08d94907e00568ccc92dc46e8f88
unknown <dow@college.harvard.edu>
2019-04-18 14:29:56 -0400

    a

--- script.R
+++ script.R
@@ -3,10 +3,13 @@ library(tidycensus)
 library(mapview)
 library(sf)
 library(leaflet)
+library(tigris)
 api_key <- "ad6d908d59b8fd6b7cd711901f628f9060feb972"
 census_api_key(api_key)
 palm_beach <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/04/palmbeachcounty_fl.csv")
-shapes <- urban_areas(class = "sf")
+
+fl <- counties(state = "FL", class = "sf")
+fl <-  fl[fl$NAME == "Palm Beach",]
 
 
-
+fl %>% ggplot() + geom_sf()
