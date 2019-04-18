369f55c4bc4f8c7cdcebb914422e7d2dcbd8503b
unknown <dow@college.harvard.edu>
2019-04-18 14:04:28 -0400

    a

--- script.R
+++ script.R
@@ -1,4 +1,14 @@
 library(tidyverse)
 
-palm_beach <- read.csv("http://justicetechlab.org/wp-content/uploads/2019/04/palmbeachcounty_fl.csv")
+palm_beach <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/04/palmbeachcounty_fl.csv",
+                       cols_types = cols(
+                         address = col_character(),
+                         city = col_character(),
+                         state = col_character(),
+                         datetime = col_character(),
+                         numrounds = col_double(),
+                         shotspotterflexid = col_double(),
+                         lat = col_double(),
+                         long = col_double()
+                       ))
 
