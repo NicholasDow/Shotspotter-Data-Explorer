235565c11c9cb3495bd51fc0272fff2986437b19
unknown <dow@college.harvard.edu>
2019-04-18 14:12:08 -0400

    ad

--- script.R
+++ script.R
@@ -4,16 +4,9 @@ library(mapview)
 library(sf)
 library(leaflet)
 api_key <- "ad6d908d59b8fd6b7cd711901f628f9060feb972"
+census_api_key(api_key)
+palm_beach <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/04/palmbeachcounty_fl.csv")
+shapes <- urban_areas(class = "sf")
 
-palm_beach <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/04/palmbeachcounty_fl.csv",
-                       cols_types = cols(
-                         address = col_character(),
-                         city = col_character(),
-                         state = col_character(),
-                         datetime = col_character(),
-                         numrounds = col_double(),
-                         shotspotterflexid = col_double(),
-                         lat = col_double(),
-                         long = col_double()
-                       ))
+
 
