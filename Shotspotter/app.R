c7883fae6ee64b90af0a71b051473fd229aa54a5
unknown <dow@college.harvard.edu>
2019-04-21 02:56:13 -0400

    Really basic changes, want to look at shiny app winners to make code better, submit this if all else fails

--- Shotspotter/app.R
+++ Shotspotter/app.R
@@ -14,24 +14,35 @@ library(mapview)
 library(sf)
 library(leaflet)
 library(tigris)
-Goldsboro <- read_rds("Goldsboro.rds")
+api_key <- "ad6d908d59b8fd6b7cd711901f628f9060feb972"
+census_api_key(api_key)
+Goldsboro <- read_csv("http://justicetechlab.org/wp-content/uploads/2019/04/goldsboro_nc.csv")
+raw_shapes <- urban_areas(class = "sf")
+
+shapes <- raw_shapes %>% 
+  filter(NAME10 == "Goldsboro, NC")
+
+# do this manipulation in the app
+
+
+type_shot <- Goldsboro %>% distinct(type)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    # Application title
-   titlePanel(""),
+   titlePanel("Mapping type of shot in Goldsboro 2018<- I dont know if that is right"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         checkboxInput("Type ",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
+        checkboxGroupInput(inputId = "type_gun", 
+                    choices = type_shot$type,
+                    label = "Type of Shot Spotted", 
+                    selected = type_shot$type[[1]]),
+        checkboxGroupInput(inputId = "year",
+                    label = "years", 
+                    choices = c(2016,2017,2018,2019),
+                    selected = 2016)),
       mainPanel(
          plotOutput("distPlot")
       )
@@ -41,13 +52,19 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$distPlot <- renderPlot({ 
+     show <- Goldsboro %>% 
+       filter(type == input$type_gun) %>% 
+       filter(year == input$year)
+     
+     Goldsboro_locations <- st_as_sf(show, 
+                                     coords = c("longitude", "latitude"), 
+                                     crs = 4326)
+     shapes %>% 
+       ggplot() + 
+       geom_sf()+ 
+       geom_sf(data = Goldsboro_locations, mapping = aes(color = type)) +
+       ggtitle("Type of shot in the year", input$year)
    })
 }
 
